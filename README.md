a5c44211baeb2a30096cac1e8c6ce79ec9bbaed0
Janusz Hatala <janusz.hatala@capsilon.com>
2018-10-24 10:51:52 +0200

    Swagger 2.0 added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java-gson-samples
 Java Spring Boot sample project with an example of reading JSON with gson library.
+
+Swagger 2.0 included. 
+To see app endpoinds type:<br>
+    ``localhost:8080/v2/api-docs``<br>
+    ``localhost:8080/swagger-ui.html``<br><br>
+------
--- src/main/java/com/hatala/jhdemo8json/HomeController.java
+++ src/main/java/com/hatala/jhdemo8json/HomeController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@JsonGetMapping("/")
 	public String home() {
-		return "View Role\n" +
+		return "View endpoints:\n" +
+				"localhost:8080/v2/api-docs\n" +
+				"\n" +
+				"View Swagger UI:\n" +
+				"localhost:8080/swagger-ui.html\n" +
+				"\n" +
+				"View Role\n" +
 				"localhost:8080/workspaces/Role1\n" +
 				"localhost:8080/workspaces/Role2\n" +
 				"localhost:8080/workspaces/Role3\n" +
